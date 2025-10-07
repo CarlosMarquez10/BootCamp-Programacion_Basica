f60cf64f73f0b687628112a3a6bac25e8f7f83c0
Carlos David Marquez Valencia <carlos.marquez@inmel.co>
2025-10-06 22:59:40 -0500

    se agrega los concepto de la finction normal

---
+++ Semana_02/function_normales.js
@@ -0,0 +1,11 @@
+//La funciones es un bloque de código que realiza una tarea especifica y se puede reutilizar en cualquier en cualquiere parte del programa.
+
+function saludar(){   // en esta linea por medio de la palabra reservado function iniciamos, el segundo dato es el nombre de la funcion saludar seguido de dos parentesis, luego se abren la llave, dentro de esta va el codigo que se ejecutara cada ves que la function se llame. 
+
+    console.log("Hola, carlos!"); // con estaline u el metodo console.log lo que hacemos es imprimer, y ver los valores por consola.
+} // aqui termina, y es el cirre de la function.
+
+saludar(); // al escribor esto lo que hacemos es llamar la function, y esta se ejecutara automatica mente al correr el script
+
+
+
