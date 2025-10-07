52b3460e01bec445421468213c8cdc6142743738
Carlos David Marquez Valencia <carlos.marquez@inmel.co>
2025-10-07 17:30:38 -0500

    se realiza la explicacion de la funcion con parametro

--- Semana_02/function_parametro.js
+++ Semana_02/function_parametro.js
@@ -1,1 +1,15 @@
-// las funciones de con parametros 
+// las funciones de con parametros es una funcion que recibe valores llamado argumentos cuando se llama, y los usa dentro de su cuerpo para realizar alguna operacion.
+
+
+// explicacion del la funcion
+
+function saludar(nombre){ // la variable nombre aqui en esta posicion seria el parametro
+    console.log("Hola, "+ nombre + "...!")
+}
+
+// cuando llamamos la funcion de la siguiente manera
+
+saludar("carlos"); // el valor que le estamos pasando se ve como argumento y no como parametro. 
+
+
+
