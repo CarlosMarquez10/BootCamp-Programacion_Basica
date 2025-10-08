77b52f472eef416fc3747a42e949ab856a3e9de3
Carlos David Marquez Valencia <carlos.marquez@inmel.co>
2025-10-07 21:01:16 -0500

    se agregan variables para probar el metodo typeof

---
+++ operadores_logicos.js
@@ -0,0 +1,10 @@
+let dato_1 = 12;
+let dato_2 = 23;
+let dato_3 = 12;
+
+
+console.log(dato_1 && dato_2)
+console.log(dato_3 || dato_2)
+console.log(dato_1 != dato_2)
+
+
---
+++ typeof.js
@@ -0,0 +1,14 @@
+let number = 123;
+let dato = "carlos"
+let bolean = true
+let data = {}
+let data_x = []
+let datadecimal = 3.443;
+
+
+console.log(typeof number) // se tiene que es un dato de tipo namber
+console.log(typeof dato) // se tiene que es un dato de tipo string
+console.log(typeof bolean) // se tiene que es un dato de tipo boolean
+console.log(typeof data) // se tiene que es un tipo de datos objeto
+console.log(typeof data_x) // para los array podemos ver que es tambien es un dato de tipo objeto
+console.log(typeof datadecimal) // tenemos que para los datos de tipo decimal son de tipo nember
